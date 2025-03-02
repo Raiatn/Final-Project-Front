11b9cea731d9d9e18ccf316005b5ca327ac8c152
Raiatn <raika.ataeeyan83@gmail.com>
2025-03-02 14:35:01 +0330

    Update Header.jsx

--- src/components-of-the page/Header.jsx
+++ src/components-of-the page/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <>
     <div className='w-full bg-white flex p-1  border-s-gray-300 border-2 shadow'>
-    <div className="w-80 "><img className="min-w-full" src="src\assets\Logo.png" alt="Logo" /></div>
+    <div className="w-80 "><img className="min-w-full" src="dist\assets\Logo.png" alt="Logo" /></div>
    
       </div>
     </>
